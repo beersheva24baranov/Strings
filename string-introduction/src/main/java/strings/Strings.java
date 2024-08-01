0c174b1a41cc0981fb3d67d6e7e466db07c1dc14
AlexanderBaranov <avbc@mail.ru>
2024-08-01 11:11:39 +0300

    fix tests and javaVariable, add classwork

--- string-introduction/src/main/java/strings/Strings.java
+++ string-introduction/src/main/java/strings/Strings.java
@@ -1,6 +1,17 @@
 package strings;
 
+import java.util.Arrays;
+
 public class Strings {
+        static final String keyWords[] = { "abstract", "assert", "boolean",
+                    "break", "byte", "case", "catch", "char", "class", "const",
+                    "continue", "default", "do", "double", "else", "enum", "extends", "false",
+                    "final", "finally", "float", "for", "goto", "if", "implements",
+                    "import", "instanceof", "int", "interface", "long", "native",
+                    "new", "null", "package", "private", "protected", "public",
+                    "return", "short", "static", "strictfp", "super", "switch",
+                    "synchronized", "this", "throw", "throws", "transient", "true",
+                    "try", "void", "volatile", "while" };
     public static String firstName() {
         //regex for strings starting with capital letter and rest as lowercase letters
         //minimal length is 5 letters
@@ -10,6 +21,56 @@ public class Strings {
         //TODO
         //regular expression for testing syntax of Java veriable names
         //only ACII symbols are allowed
-        return "^[a-z][a-zA-Z0-9]*$";
+        return "((?!_$)[a-zA-Z$_][\\w$]*)";
     }
+    public static String number0_300() {
+       
+        return "[1-9]\\d?|[1-2]\\d\\d|300|0";
+    }
+    public static String ipV4Octet(){
+        
+        return "([0-1]?\\d{1,2}|2([0-4]\\d|5[0-5]))";
+    }
+    public static String ipV4Address(){
+        String octetExpr = ipV4Octet();
+        return String.format("%s(\\.%s){3}", octetExpr, octetExpr);
+    }
+    public static String stringWithJavaNames(String names) {
+       String [] tokens = names.split("\\s+");
+       int i = getJavaNameIndex(tokens, -1);
+       String res = "";
+       if (i >= 0) {
+         res = tokens[i];
+       }
+       while((i = getJavaNameIndex(tokens, i)) > 0) {
+            res += " " + tokens[i];
+       }
+       
+        return res;
+    }
+
+    private static int getJavaNameIndex(String[] tokens, int i) {
+        i++;
+        while(i < tokens.length && !isJavaName(tokens[i])) {
+            i++;
+        }
+        return i < tokens.length ? i : -1;
+    }
+
+    private static boolean isJavaName(String string) {
+        
+        return string.matches(javaVariable()) && Arrays.binarySearch(keyWords, string) < 0;
+}
+public static boolean isArithmeticExpression(String expr) {
+    //TODO
+    //1. brackets
+    //right position of open / close bracket is matter of regex
+    //matching between open and close bracket is matter of the method you are supposed to write
+    //based on a counter. If counter is negative - no matching;
+    // if at ending up going through a string the counter doesn't equal 0 - no matching
+    //matching may be only in one case: at the ending up of going the counter will be 0
+    // Operator - regular expression for one out of 4 arithemetic operators [*/+-]
+    //Operand may be either Java variable name or number (better any)
+    return false;
+}
 }
--- string-introduction/src/test/java/strings/RegularExpressionsTest.java
+++ string-introduction/src/test/java/strings/RegularExpressionsTest.java
@@ -1,5 +1,6 @@
 package strings;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static strings.Strings.*;
@@ -8,34 +9,104 @@ import org.junit.jupiter.api.Test;
 public class RegularExpressionsTest {
     @Test
     void javaVariableTest() {
-        // TODO
-        assertTrue("varname".matches(javaVariable()));
+            assertTrue("varname".matches(javaVariable()));
         assertTrue("varName".matches(javaVariable()));
         assertTrue("varnamEvvijvji".matches(javaVariable()));
         assertTrue("varNAme".matches(javaVariable()));
         assertTrue("floattt".matches(javaVariable()));
         assertTrue("int12".matches(javaVariable()));
         assertFalse("varNAme ".matches(javaVariable()));
-        assertFalse("String_".matches(javaVariable()));
-        assertFalse("___".matches(javaVariable()));
-        assertFalse("___123".matches(javaVariable()));
-        assertFalse("$$$$".matches(javaVariable()));
+        assertTrue("String_".matches(javaVariable()));
+        assertTrue("___".matches(javaVariable()));
+        assertTrue("___123".matches(javaVariable()));
+        assertTrue("$$$$".matches(javaVariable()));
         assertFalse("{___}".matches(javaVariable()));
         assertFalse("varname[_".matches(javaVariable()));
         assertFalse("|".matches(javaVariable()));
         assertFalse("1".matches(javaVariable()));
         assertFalse("1Q".matches(javaVariable()));
-        assertFalse("Z".matches(javaVariable()));
+        assertTrue("Z".matches(javaVariable()));
         assertFalse("".matches(javaVariable()));
         assertFalse("__||".matches(javaVariable()));
         assertFalse("_".matches(javaVariable()));
-        assertFalse("$varname".matches(javaVariable()));
-        assertFalse("_varname".matches(javaVariable()));
-        assertFalse("$$$__varname".matches(javaVariable()));
-        assertFalse("var_Name".matches(javaVariable()));
-        assertFalse("var_n_ame".matches(javaVariable()));
-        assertFalse("varname_$".matches(javaVariable()));
-        assertFalse("A1".matches(javaVariable()));
-        assertFalse("_$$$$A1".matches(javaVariable()));
+        assertTrue("$varname".matches(javaVariable()));
+        assertTrue("_varname".matches(javaVariable()));
+        assertTrue("$$$__varname".matches(javaVariable()));
+        assertTrue("var_Name".matches(javaVariable()));
+        assertTrue("var_n_ame".matches(javaVariable()));
+        assertTrue("varname_$".matches(javaVariable()));
+        assertTrue("A1".matches(javaVariable()));
+        assertTrue("_$$$$A1".matches(javaVariable()));
+    }
+    @Test
+    void number0_300TrueTest(){
+        String regex = Strings.number0_300();
+        assertTrue("0".matches(regex));
+        assertTrue("300".matches(regex));
+        assertTrue("250".matches(regex));
+        assertTrue("25".matches(regex));
+        assertTrue("12".matches(regex));
+        assertTrue("299".matches(regex));
+        assertTrue("1".matches(regex));
+
+    }
+    @Test
+    void number0_300FalseTest(){
+        String regex = Strings.number0_300();
+        assertFalse("00".matches(regex));
+        assertFalse("301".matches(regex));
+        assertFalse("01".matches(regex));
+        assertFalse("00".matches(regex));
+        assertFalse("1(".matches(regex));
+        assertFalse("1000".matches(regex));
+        assertFalse(" 20".matches(regex));
+        assertFalse("1001".matches(regex));
+    }
+    @Test
+    void ipV4OctetTrueTest(){
+        String regex = Strings.ipV4Octet();
+        assertTrue("0".matches(regex));
+        assertTrue("00".matches(regex));
+        assertTrue("000".matches(regex));
+        assertTrue("10".matches(regex));
+        assertTrue("100".matches(regex));
+        assertTrue("255".matches(regex));
+        assertTrue("199".matches(regex));
+        assertTrue("249".matches(regex));
+    }
+    @Test
+    void ipV4OctetFalseTest(){
+        String regex = Strings.ipV4Octet();
+        assertFalse("0000".matches(regex));
+        assertFalse("t".matches(regex));
+        assertFalse("-1".matches(regex));
+        assertFalse("1111".matches(regex));
+        assertFalse("0001".matches(regex));
+        assertFalse("256".matches(regex));
+        assertFalse("300".matches(regex));
+        assertFalse("*".matches(regex));
+        assertFalse("1 ".matches(regex));
+    }
+    @Test
+    void ipV4AddressTrueTest() {
+        String regex = Strings.ipV4Address();
+        assertTrue("0.0.0.0".matches(regex));
+        assertTrue("255.255.255.255".matches(regex));
+    }
+    @Test
+    void ipV4AddressFalseTest() {
+        String regex = Strings.ipV4Address();
+        assertFalse("0.0.0".matches(regex));
+        assertFalse("0.0.0+0".matches(regex));
+        assertFalse("0".matches(regex));
+        assertFalse("0.-".matches(regex));
+        assertFalse("0.0.0*0".matches(regex));
+        assertFalse("0.0.0 0".matches(regex));
+    }
+    @Test
+    void stringWithJavaNamesTest() {
+        String names = "123 1a _ abs int enum null lmn";
+        String expected = "abs lmn";
+        assertEquals(expected, Strings.stringWithJavaNames(names));
     }
 }
